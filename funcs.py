01043c170f1841e0df556c21d6062b8089c8049d
Federico <fvalenciao95@gmail.com>
2022-01-30 21:49:07 +0100

    partial interaction with local database

--- funcs.py
+++ funcs.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from db import execute_query
+
+def get_first_order_id(table_name, id_name):
+    first_id_dict = execute_query(f'SELECT {id_name} FROM {table_name} ORDER BY {id_name} DESC LIMIT 1')
+    
+    if first_id_dict == ():
+        first_id = 1
+    else:
+        first_id = int(first_id_dict[0][id_name]) + 1
+    
+    return first_id
 
 def get_prod_id(df, product):
     
@@ -19,19 +30,28 @@ def get_prod_id(df, product):
         
     return prod_id
 
-def get_id(df, branch, id_name):
-    try:
-        branch_id = df.loc[branch][id_name]
-    except:
-        if len(df[id_name]) > 0:
-            branch_id = df.iloc[-1,0] + 1
-        else:
-            branch_id = 1
-        
-        df.loc[branch] = [branch_id]
+def get_prod_id_sql(table_name, product, id_name,var_name,price,price_name):
     
+    prod_id_dict = execute_query(f'SELECT {id_name} FROM {table_name} WHERE {var_name} = "{product}"')
+    
+    if prod_id_dict == ():
+        execute_query(f'INSERT INTO {table_name} ({var_name}, {price_name}) VALUES ("{product}", {price})')
+        prod_id_dict = execute_query(f'SELECT {id_name} FROM {table_name} WHERE {var_name} = "{product}"')
+    
+    prod_id = prod_id_dict[0][id_name]
+    return prod_id
+
+def get_id_sql(table_name, branch, id_name, var_name):
+
+    branch_id_dict = execute_query(f'SELECT {id_name} FROM {table_name} WHERE {var_name} = "{branch}"')
+    
+    if branch_id_dict == (): # branch doesn't exist yet
+        execute_query(f'INSERT INTO {table_name} ({var_name}) VALUES ("{branch}")')
+        branch_id_dict = execute_query(f'SELECT {id_name} FROM {table_name} WHERE {var_name} = "{branch}"')
+    
+    branch_id = branch_id_dict[0][id_name]
     return branch_id
-    
+
 def split(strng, sep, pos):
     strng = strng.split(sep)
     return sep.join(strng[:pos]), sep.join(strng[pos:])
--- main.py
+++ main.py
@@ -10,32 +10,59 @@ filepath = 'chesterfield.csv'
 orders_df = pd.read_csv(filepath)
 orders_df.columns = ["Timestamp", "Location", "Customer Name", "Order", "Total Cost", "Payment Method", "Card Number"]
 
-order_ids = np.linspace(1,len(orders_df['Timestamp']), len(orders_df['Timestamp']))
+first_id = get_first_order_id('transactions', 'OrderID')
+order_ids = np.linspace(first_id,first_id + len(orders_df['Timestamp']), len(orders_df['Timestamp']))
 order_ids = order_ids.tolist()
 order_ids = [int(i) for i in order_ids]
 
 orders_df['Order ID'] = order_ids
 orders_df.set_index('Order ID', inplace=True)
 
+
 #%%
 # DROP SENSITIVE INFO
 orders_df.drop(['Customer Name', 'Card Number'], axis=1, inplace=True)
 
 #%%
-# CREATE AND POPULATE PRODUCTS AND ORDER PRODUCTS DATAFRAMES
+# CREATE AND POPULATE BRANCH ID COLUMN
+
+branches = orders_df['Location'].tolist()
+order_branch_ids = []
+
+for branch in branches:
+    order_branch_ids.append(get_id_sql('locations', branch, 'BranchID', 'BranchName'))
+
+# REPLACE BRANCH NAME WITH BRANCH ID IN ORDERS DATAFRAME
+orders_df['Branch ID'] = order_branch_ids
+orders_df.drop(['Location'], axis=1, inplace=True)
+
+#%%
+# CREATE AND POPULATE PAYMENT METHODS COLUMN
+
+payments = orders_df['Payment Method'].tolist()
+order_payment_ids = []
+
+for payment in payments:
+    order_payment_ids.append(get_id_sql('payment_method', payment, 'PaymentID', 'PaymentMethod'))
+
+# REPLACE PAYMENT NAME WITH PAYMENT ID IN ORDERS DATAFRAME
+orders_df['Payment ID'] = order_payment_ids
+orders_df.drop(['Payment Method'], axis=1, inplace=True)
+
+#%%
+# CREATE AND POPULATE ORDER PRODUCTS DATAFRAME, UPDATE PRODUCTS TABLE
 
 order_prods_df = pd.DataFrame(columns=['Order ID', 'Product ID', 'Quantity', 'Price Paid'])
-prods_df = pd.DataFrame(columns=['Product ID', 'Product Name', 'Current Price'])
-prods_df.set_index('Product Name', inplace=True)
 
 order_ids_prods = []
 order_prods_ids = []
 order_prods_quantity = []
 order_prods_prices = []
 
-for i in range(len(order_ids)):
+for i in order_ids:
     
-    order_prods = orders_df['Order'].iloc[i].split(',')
+    print(i-first_id-1)
+    order_prods = orders_df['Order'].iloc[i-first_id-1].split(',')
     
     for order_prod in order_prods:
         
@@ -47,7 +74,7 @@ for i in range(len(order_ids)):
         prod = order_prod[0]
         price = float(order_prod[1])
         
-        prod_id = get_prod_id(prods_df, order_prod)
+        prod_id = get_prod_id_sql('products',prod,'ProdID','ProdName',price,'CurrentPrice')
         
         order_ids_prods.append(i)
         order_prods_ids.append(prod_id)
@@ -62,39 +89,4 @@ order_prods_df['Price Paid'] = order_prods_prices
 order_prods_df.set_index('Order ID', inplace=True)
 
 orders_df.drop(['Order'], axis=1, inplace=True)
-
-#%%
-# CREATE AND POPULATE LOCATIONS DATAFRAME
-
-locations_df = pd.DataFrame(columns=['Branch ID', 'Branch Name'])
-locations_df.set_index('Branch Name', inplace=True)
-
-branches = orders_df['Location'].tolist()
-order_branch_ids = []
-
-for branch in branches:
-    order_branch_ids.append(get_id(locations_df, branch, 'Branch ID'))
-
-# REPLACE BRANCH NAME WITH BRANCH ID IN ORDERS DATAFRAME
-orders_df['Branch ID'] = order_branch_ids
-orders_df.drop(['Location'], axis=1, inplace=True)
-
-#%%
-# CREATE AND POPULATE PAYMENT METHODS DATAFRAME
-
-payment_df = pd.DataFrame(columns=['Payment ID', 'Payment Name'])
-payment_df.set_index('Payment Name', inplace=True)
-
-payments = orders_df['Payment Method'].tolist()
-order_payment_ids = []
-
-for payment in payments:
-    order_payment_ids.append(get_id(payment_df, payment, 'Payment ID'))
-
-# REPLACE BRANCH NAME WITH BRANCH ID IN ORDERS DATAFRAME
-orders_df['Payment ID'] = order_payment_ids
-orders_df.drop(['Payment Method'], axis=1, inplace=True)
-print(orders_df.head())
-print(payment_df.head())
-print(locations_df.head())
-print(order_prods_df.head())
+# %%
