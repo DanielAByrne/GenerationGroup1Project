236ccc66cf6cdfb784accff25dc784503441ab74
DanielAByrne <danielbyrne20@yahoo.co.uk>
2022-02-18 16:18:04 +0000

    new lambda function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -15,4 +15,5 @@ def handler(event, context):
     s3 = boto3.resource('s3')
     s3.meta.client.download_file(bucket, key, f'/tmp/{filename}')
     
-    main_etl(filename)
+    main_etl(f'/tmp/{filename}')
+
